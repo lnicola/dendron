311eb2b1c7941726ea1e14b437d42dc501c04f7e
Kevin <kevin@dendron.so>
2021-09-28 12:10:08 -0700

    Revert "enhance(publish): filter body out of generated notes.json for next (#1380)"
    This reverts commit 2ba8f0c1c2d1035f249baabccf7e25dea013f7b1.

--- packages/pods-core/src/builtin/NextjsExportPod.ts
+++ packages/pods-core/src/builtin/NextjsExportPod.ts
@@ -22,15 +22,6 @@ type NextjsExportPodCustomOpts = {
   overrides?: Partial<DendronSiteConfig>;
 };
 
-export const mapObject = (
-  obj: { [k: string]: any },
-  fn: (k: string, v: any) => any
-) => Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, fn(k, v)]));
-
-export const removeBodyFromNote = ({ body, ...note }: Record<string, any>) => note
-
-export const removeBodyFromNotesDict = (notes: NotePropsDict) => mapObject(notes, (_k, note: NotePropsDict) => removeBodyFromNote(note))
-
 function getSiteConfig({
   siteConfig,
   overrides,
@@ -279,14 +270,7 @@ export class NextjsExportPod extends ExportPod<NextjsExportConfig> {
     );
     const podDstPath = path.join(podDstDir, "notes.json");
     const podConfigDstPath = path.join(podDstDir, "dendron.json");
-    fs.writeJSONSync(
-      podDstPath,
-      {
-        ...payload,
-        notes: removeBodyFromNotesDict(payload.notes),
-      },
-      { encoding: "utf8", spaces: 2 }
-    );
+    fs.writeJSONSync(podDstPath, payload, { encoding: "utf8", spaces: 2 });
     fs.writeJSONSync(podConfigDstPath, engineConfig, {
       encoding: "utf8",
       spaces: 2,
